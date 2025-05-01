7c08412db463f6ebd84e579bc10e8f912a1a55c7
Mohammed Fareed <mohdf.fareed@icloud.com>
2025-05-01 00:12:03 +0000

    Refactor build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,37 +1,60 @@
 #!/bin/sh
 
+# MARK: Arguments =============================================================
+
 # help message
 USAGE="usage: $0 [path=.bin]"
 if [ "$#" -gt 1 ]; then echo "$USAGE" && exit 1; fi
 
-# arguments
 APP=./chatgpt # the app source code
 BIN="${1:-.bin}" # the binaries path
+
 exec=$(basename "$APP") # the executable
 out="$BIN/$exec" # the output path
+
+# MARK: Build =================================================================
 
 # build for a plat-arch and package it
 build() { # usage: build <plat> <arch> <id>
   plat="$1"; arch="$2"; id="$3"
+  output="$out"
+
+  # add .exe for windows
+  if [ "$plat" = "windows" ]; then
+    output="$output.exe"
+  fi
 
   # build the executable
   echo "building for $id..."
-  GOOS=$plat GOARCH=$arch go build -o "$out" "$APP"
+  GOOS=$plat GOARCH=$arch go build -o "$output" "$APP"
 
   # package into an archive
   archive="$out-$id.zip"
-  zip -j "$archive" "$out" > /dev/null
+  zip -j "$archive" "$output" > /dev/null
 }
+
+# MARK: Targets ===============================================================
 
 # linux
 build linux arm64 "linux-arm"
 build linux amd64 "linux-x64"
+
 # macos
 build darwin arm64 "macos-arm"
 build darwin amd64 "macos-x64"
+
 # windows
 build windows arm64 "win-arm"
 build windows amd64 "win-x64"
 
-# cleanup
-rm "$out"
+# development
+echo "building for debug..."
+if [ "$(go env GOOS)" = "windows" ]; then
+  go build -tags=debug -o "$out.exe" "$APP"
+  rm "$out"
+else # macos | linux
+  go build -tags=debug -o "$out" "$APP"
+  rm "$out.exe"
+fi
+
+echo "builds created in $BIN"
